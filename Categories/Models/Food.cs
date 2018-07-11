f26caabd890ffbb7d0fd25a1283c3613072bf94e
Devin Mounts and Ryan Putman <ryan@>
2018-07-11 10:50:17 -0700

    initial commit

---
+++ Categories/Models/Food.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+namespace Categories.Models
+{
+    public class Food
+    {
+        private int _id;
+        private string _name;
+        private int _categoryId;
+ 
+
+        public Food(int id, int categoryId, string name)
+        {
+            _id = id;
+            _categoryId = categoryId;
+            _name = name;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public int GetCategoryId()
+        {
+            return _categoryId;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public static List<Food> GetAll()
+        {
+            List<Food> allFoods = new List<Food> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM foods;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read())
+            {
+                int Id = rdr.GetInt32(0);
+                int CategoryId = rdr.GetInt32(1);
+                string Name = rdr.GetString(2);
+                Food newFood = new Food(Id, CategoryId, Name);
+                allFoods.Add(newFood);
+            }    
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allFoods;   
+        }
+
+        public override bool Equals(System.Object otherFood)
+        {
+
+            if (!(otherFood is Food))
+            {
+                return false;
+            }
+            else
+            {
+                Food newFood = (Food)otherFood;
+                bool idEquality = (this.GetId() == newFood.GetId());
+                bool nameEquality = (this.GetName() == newFood.GetName());
+                return (idEquality && nameEquality);
+            }
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO `foods` (`id`, `category_id`, `name`) VALUES (@thisId, @thisCategory_Id, @thisName);";
+
+            MySqlParameter id = new MySqlParameter();
+            id.ParameterName = "@thisId";
+            id.Value = this._id;
+
+            MySqlParameter category_id = new MySqlParameter();
+            category_id.ParameterName = "@thisCategory_Id";
+            category_id.Value = this._categoryId;
+
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = "@thisName";
+            name.Value = this._name;
+
+            cmd.Parameters.Add(id);
+            cmd.Parameters.Add(category_id);
+            cmd.Parameters.Add(name);
+
+            cmd.ExecuteNonQuery();
+            _id = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetName().GetHashCode();
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM foods;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static Food Find(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM `foods` WHERE id = @thisId;";
+
+            MySqlParameter thisId = new MySqlParameter();
+            thisId.ParameterName = "@thisId";
+            thisId.Value = id;
+            cmd.Parameters.Add(thisId);
+
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+            int Id = 0;
+            int CategoryId = 0;
+            string Name = "";
+
+            while (rdr.Read())
+            {
+                int Id = rdr.GetInt32(0);
+                int CategoryId = rdr.GetInt32(1);
+                string Name = rdr.GetString(2);
+            }
+
+            Food foundItem = new Food(id, CategoryId, Name);  // This line is new!
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+
+            return foundItem;  // This line is new!
+
+
+    }
+}
---
+++ Category.Tests/ModelsTests/FoodTests.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Categories.Models;
+
+namespace Categories.Tests
+{
+    [TestClass]
+    public class CategoryTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Food.DeleteAll();
+        }
+        public void FoodTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=relations;";
+        }
+
+        [TestMethod]
+        public void GetSetName_SetsGetsName_String()
+        {
+            Food newFood = new Food(1, 2,  "apple");
+
+            Assert.AreEqual("apple", newFood.GetName());
+        }
+
+        [TestMethod]
+        public void GetSetId_SetsGetsId_Int()
+        {
+            Food newFood = new Food(1, 2, "apple");
+
+            Assert.AreEqual(1, newFood.GetId());
+        }
+
+        [TestMethod]
+        public void GetSetCategoryId_SetsGetsCategoryId_Id()
+        {
+            Food newFood = new Food(1, 2, "apple");
+
+            Assert.AreEqual(2, newFood.GetCategoryId());
+
+        }
+
+        [TestMethod]
+        public void GetAll_DbStartsEmpty_0()
+        {
+            int result = Food.GetAll().Count;
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Equals_ReturnsTrueIfSameIds_Bool()
+        {
+            Food firstFood = new Food(1, 2, "apple");
+            Food secondFood = new Food(1, 2, "apple");
+
+            Assert.AreEqual(firstFood, secondFood);
+        }
+
+        [TestMethod]
+        public void Save_SaveFoodToDatabase_FoodList()
+        {
+            Food testFood = new Food(1, 2, "apple");
+
+            testFood.Save();
+            List<Food> result = Food.GetAll();
+            List<Food> testList = new List<Food> { testFood };
+
+            CollectionAssert.AreEqual(testList, result);
+        }
+
+        [TestMethod]
+        public void Save_AssignsIdToObject_Id()
+        {
+            Food testFood = new Food(1, 2, "apple");
+
+            testFood.Save();
+            Food savedFood = Food.GetAll()[0];
+
+            int result = savedFood.GetId();
+            int testId = testFood.GetId();
+
+            Assert.AreEqual(testId, result);
+        }
+
+    }
+}
